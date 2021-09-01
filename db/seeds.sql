7f59284e5f8c11c00928fbf5c930718ccf75351f
Dan Possehl <danielbpossehl@gmail.com>
2021-09-01 16:35:51 -0600

    was able to get seeds to populate table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (id, deparment_name)
+VALUES ()
+
+INSERT INTO employee_roll(id, title, salary, deparment_id)
+Values ()
+
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES()
